0561593630bf342abd07235ba8fb7ee06f94a648
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2026-03-17 09:51:19 +0300

    edit api session repo

--- lib/data/repos/session_repo/api_session_repo.dart
+++ lib/data/repos/session_repo/api_session_repo.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:f1_dashboard/core/config.dart';
 import 'package:f1_dashboard/data/models/session.dart';
@@ -7,22 +9,55 @@ import 'package:hive/hive.dart';
 class ApiSessionRepo extends SessionRepo {
   final String _apiUrl = '${Config.baseApiUrl}/sessions';
   final _dio = Dio();
+  final _box = Hive.box(name: 'cache-sessions');
 
-  final _box = Hive.box(name: 'cache');
+  ApiSessionRepo() {
+    log('init repo', name: 'api_session_repo');
+  }
 
   @override
   Future<List<Session>> getSessions(int year) async {
+    log('start geting repo', name: 'api_session_repo');
+    final String yearTag = year.toString();
+    List<Session>? data;
+
     if (_box.isNotEmpty) {
-      final cache = _box.get('sessions') as List<dynamic>;
-      final List<Session> data = cache.map((e) => Session.fromJson(e)).toList();
-      return data;
-    } else {
-      final response = await _dio.get('$_apiUrl?year=$year');
-      final data = (response.data as List)
-          .map((e) => Session.fromJson(e))
-          .toList();
-      _box.put('sessions', data);
-      return data;
+      log('cheak cache', name: 'api_session_repo');
+      data = await _fetchFromCache(yearTag);
     }
+
+    if (data == null) {
+      log('cache empty', name: 'api_session_repo');
+      data = await _fetchFromApi(yearTag);
+      _loadCache(yearTag, data.map((e) => e.toJson()).toList());
+    }
+
+    log('returning ${data.length} items', name: 'api_session_repo');
+    return data;
+  }
+
+  Future<void> _loadCache(
+    String yearTag,
+    List<Map<String, dynamic>> data,
+  ) async {
+    _box.put(yearTag, data);
+  }
+
+  Future<List<Session>?> _fetchFromCache(String yearTag) async {
+    final cache = _box.get(yearTag) as List<dynamic>?;
+    return cache?.map((e) => Session.fromJson(e)).toList();
+  }
+
+  Future<List<Session>> _fetchFromApi(String yearTag) async {
+    final fullUrl = '$_apiUrl?year=$yearTag';
+
+    log('calling api -> $fullUrl', name: 'api_session_repo');
+
+    final response = await _dio.get(fullUrl);
+    final data = (response.data as List)
+        .map((e) => Session.fromJson(e))
+        .toList();
+    _box.put('sessions', data);
+    return data;
   }
 }
